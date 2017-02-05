05c13f8473c1db5b791aa50962768fdaa92fee31
kushmisra <kushmisra2609@gmail.com>
2017-02-06 00:14:21 +0530

    onfire

--- main.rb
+++ main.rb
@@ -1,17 +1,38 @@
-require 'watir-webdriver'
 
-b = Watir::Browser.new:chrome
-b.goto 'https://www.facebook.com'
-b.text_field(id: 'email').set 'kushmisra2609@gmail.com'
-b.text_field(id: 'pass').set 'password'
-b.button(type: 'submit').when_present.click
+require 'watir'
 
-
-b.text_field(placeholder: 'Search').set 'mayank kumar'
-sleep 4
-b.send_keys :enter
-sleep 4
-if b.div(label: 'Mayank Kumar').when_present 
-  b.send_keys [:control, "a"]
+browser = Watir::Browser.new :chrome
+browser.goto 'https://www.facebook.com'
+browser.text_field(:id => 'email').set 'mk9990143266@gmail.com'
+browser.text_field(:id => 'pass').set 'password'
+browser.form(:id => 'login_form').submit
+browser.goto 'https://www.facebook.com/notifications'
+sleep 5
+names = []
+browser.divs(:class => '_4l_v').each do |div|
+	temp_text = div.span.text
+	if temp_text.include? "was"
+		next
+	end
+	if temp_text.include? "birthday"
+		temp_text = temp_text.split(' ')
+		temp_text = "#{temp_text[0]} #{temp_text[1]}"
+		names << temp_text 
+	end
 end
-s = gets 
+if names.size > 0
+	names.each do |name|
+		puts "message send to #{name}"
+	end
+end
+browser.goto (https://www.facebook.com)
+e = nil
+until e 
+	print 'Want to exit y/n '
+	s = gets.chomp
+	if s == 'y' 
+		e =1
+	else
+		e = nill
+	end
+end
