c46ff94b1b434ea43202d1c128bef17d5561cac7
kushmisra <kushmisra2609@gmail.com>
2017-02-05 22:12:36 +0530

    first comit

---
+++ main.rb
@@ -0,0 +1,14 @@
+require 'watir-webdriver'
+
+b = Watir::Browser.new:chrome
+b.goto 'https://www.facebook.com'
+b.text_field(id: 'email').set 'kushmisra2609@gmail.com'
+b.text_field(id: 'pass').set 'babita1008'
+b.button(type: 'submit').when_present.click
+
+
+b.text_field(placeholder: 'Search').set 'mayank kumar'
+sleep 2
+b.send_keys :enter
+
+s = gets 
