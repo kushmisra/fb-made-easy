92551323fa12c0cccff98f77a219b21996c0865b
kushmisra <kushmisra2609@gmail.com>
2017-02-06 00:09:42 +0530

    fc

--- main.rb
+++ main.rb
@@ -3,12 +3,15 @@ require 'watir-webdriver'
 b = Watir::Browser.new:chrome
 b.goto 'https://www.facebook.com'
 b.text_field(id: 'email').set 'kushmisra2609@gmail.com'
-b.text_field(id: 'pass').set 'babita1008'
+b.text_field(id: 'pass').set 'password'
 b.button(type: 'submit').when_present.click
 
 
 b.text_field(placeholder: 'Search').set 'mayank kumar'
-sleep 2
+sleep 4
 b.send_keys :enter
-
+sleep 4
+if b.div(label: 'Mayank Kumar').when_present 
+  b.send_keys [:control, "a"]
+end
 s = gets 
