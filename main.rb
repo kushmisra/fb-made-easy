22f0502c6edbf574a621f86e42c8e6447c0f0618
kushmisra <kushmisra2609@gmail.com>
2017-02-06 17:50:22 +0530

    ta

--- main.rb
+++ main.rb
@@ -16,7 +16,7 @@ sleep 10
 names = []
 browser.goto 'https://web.facebook.com/events/birthdays'
 browser.div(:id => 'events_birthday_view').div(:class => '_4-u2 _59ha _2fv9 _4-u8').ul(:class => '_3ng0').lis.each do |li|
-   li.div(:class => 'clearfix _3ng1').div(:class => '_3ng2 lfloat _ohe').text_field(:id => 'u_jsonp_5_a').set 'happy birthday :)'
+   li.div(:class => 'clearfix _3ng1').div(:class => '_3ng2 lfloat _ohe').textarea(:id => 'u_jsonp_5_a').set 'happy birthday :)'
    browser.send_keys :enter
    sleep 8
    names <<li.div(:class => 'clearfix _3ng1').div(:class => '_3ng2 lfloat _ohe').a.text
@@ -28,23 +28,23 @@ if names.size > 0
 	end
 end
 
-browser.goto ('https://www.facebook.com')
-names.each do |name| 
-# 100.times do
-	browser.text_field(:placeholder => 'Search').set "#{name}"
-	sleep 2
-	browser.send_keys :enter
-	sleep 2
-	element = browser.div(:class => "_1mf _1mj").span
-	message = ' na aisa bhi nahi hai...:) :P ..its because of the fests going on :)'
-	message = "arguments[0].innerHTML = '#{message}'"
-	script = "return #{message}"
-	browser.execute_script(script, element)
-	browser.send_keys [:control, "a"]
-	browser.send_keys [:control, "x"]
-	browser.send_keys [:control, "v"]
-	sleep 1
-	browser.send_keys :enter
-	browser.div(:class => 'mls titlebarButtonWrapper rfloat _ohf').a(:class => "_3olu _3olv close button").click	
-end
+# browser.goto ('https://www.facebook.com')
+# names.each do |name| 
+# # 100.times do
+# 	browser.text_field(:placeholder => 'Search').set "#{name}"
+# 	sleep 2
+# 	browser.send_keys :enter
+# 	sleep 2
+# 	element = browser.div(:class => "_1mf _1mj").span
+# 	message = ' na aisa bhi nahi hai...:) :P ..its because of the fests going on :)'
+# 	message = "arguments[0].innerHTML = '#{message}'"
+# 	script = "return #{message}"
+# 	browser.execute_script(script, element)
+# 	browser.send_keys [:control, "a"]
+# 	browser.send_keys [:control, "x"]
+# 	browser.send_keys [:control, "v"]
+# 	sleep 1
+# 	browser.send_keys :enter
+# 	browser.div(:class => 'mls titlebarButtonWrapper rfloat _ohf').a(:class => "_3olu _3olv close button").click	
+# end
 s = gets
